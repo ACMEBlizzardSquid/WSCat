9c56ba8a008ce244d584d7e0e353c68699906dab
Shake0 <housedrop@gmail.com>
2015-03-11 23:51:28 -0400

    Adding REST skeleton
    Deploying Jersey require to configure web.xml

---
+++ src/soen487/wscat/service/rest/Log.java
@@ -0,0 +1,18 @@
+package soen487.wscat.service.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("logs/{id}")
+public class Log {
+
+	//Question c
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getFileEntry(@PathParam("id") int id){
+		return Integer.toString(id);
+	}
+} 
